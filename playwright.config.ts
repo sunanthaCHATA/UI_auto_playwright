38ba850845825cc86855a8ff911d9a4cc2a19c26
sunantha <schakkingal@chata.ai>
2026-02-10 12:27:35 -0700

    New Changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,22 +25,36 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  //reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    //trace: 'on-first-retry',
 
+    trace: 'retain-on-failure',
+    // Optional: Capture video and screenshots too
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    
   },
+
+  reporter: [['html']], // Ensure the HTML reporter is active
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'],
+        launchOptions: {
+        // These prevent the runner from killing the browser process
+        handleSIGINT: false,
+        handleSIGTERM: false,
+        handleSIGHUP: false,
+      },
+      },
     },
 
     {
