c75ef3e282e637e206792e8e9c9d9d6e8f8ca13e
sunantha <schakkingal@chata.ai>
2026-03-20 15:56:27 -0600

    removed comments

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,6 @@ import { defineConfig, devices } from '@playwright/test';
 import path from 'path';
 import dotenv from 'dotenv';
 
-// ONLY load dotenv if we are NOT in Jenkins (CI)
 if (!process.env.CI) {
   dotenv.config({ path: path.resolve(__dirname, '.env') });
 }
@@ -12,21 +11,19 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   
-  /* 1. INCREASE TIMEOUT: Your logs showed 2.0m (120s) failures. 
-     The 60s timeout might be too short for Jenkins agents. */
+  
   timeout: 240000, 
 
-  /* 2. REDUCE RETRIES: Set to 1 for CI to save time. */
-  // retries: process.env.CI ? 1 : 0,
+
   retries: 0,
   workers: 1,
   reporter: [ ['html' , { open: 'never' }],
   ['allure-playwright', {
-    detail: true,                               // Shows steps in the report
-      outputFolder: 'allure-results',             // Where raw data is saved
+    detail: true,                              
+      outputFolder: 'allure-results',            
       suiteTitle: false,
   }],
-  ['list'],                                     // Optional: nice console output for Jenkins
+  ['list'],                                    
   ],
 
   
@@ -59,16 +56,5 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
-    /* 3. SHUT THESE OFF: Unless you specifically need to test 
-       Safari and Firefox every single time, comment them out. */
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
   ],
 });
